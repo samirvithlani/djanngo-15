b229ddf6dabc05bd277910d0c9d4c3d5e13a4472
samirvithlani <samir.vithlani83955@gmail.com>
2022-06-24 12:10:23 +0530

    models

--- egift/egift/settings.py
+++ egift/egift/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'product'
+    
 ]
 
 MIDDLEWARE = [
@@ -77,8 +79,13 @@ WSGI_APPLICATION = 'egift.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'internship-15',
+        'USER':'postgres',
+        'PASSWORD':'postgres',
+        'HOST':'localhost',
+        'PORT':'5432'
+
     }
 }
 
--- egift/product/models.py
+++ egift/product/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    product_name = models.CharField(max_length=25)
+    product_price = models.FloatField()
+    product_qty = models.IntegerField()
+    product_desc = models.TextField()
+    product_creation_date = models.DateTimeField()
+    product_availablity = models.BooleanField(null=True)
+
+    #product_Product
+    class Meta:
+        db_table = "product"
+
+class Country(models.Model):
+    country_name = models.CharField(max_length=25)
+    country_code = models.IntegerField()
+    country_cap = models.CharField(max_length=25)
+
+    class Meta:
+        db_table = "country"
